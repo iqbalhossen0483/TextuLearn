4411c3823b197f32025a737a21ac1d9489dfcff5
Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-06-05 22:44:49 +0600

    g env removed

--- server/config/settings.py
+++ server/config/settings.py
@@ -9,6 +9,6 @@ class Config:
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
     CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
     CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyDDAid4k6zfhCNhd49U17zgXECc7IT5s0o")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
     PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
     PINECONE_INDEX_NAME= os.getenv("PINECONE_INDEX_NAME")
